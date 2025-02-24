142522590ed49e7bd4b0e0ea275efe8109ae763d
shubham malik <malik20shubham@gmail.com>
2025-02-24 17:10:39 +0530

    third commit

--- post/server.js
+++ post/server.js
@@ -1,12 +1,24 @@
 const express=require("express");
 const app=express();
+app.use(express.urlencoded({extended:true}));
 
+let userData=[];
 
-app.get("/signup",(req,res)=>{
+app.get("/adduser",(req,res)=>{
 res.sendFile(__dirname+"/register.html");
 });
 
-
+app.post("/adduser",(req,res)=>{
+let{username,useremail,userpassword}=req.body;
+console.log(username,useremail,userpassword);
+let newUser={
+    name:username,
+    email:useremail,
+    password:userpassword
+}
+userData.push(newUser);
+res.send(userData);
+})
 
 app.listen(5001,()=>{
     console.log("server started.")
